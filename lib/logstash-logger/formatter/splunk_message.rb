d9452aad0e5e2098b466cc7961ada09cc11b7bb1
Saf1u <a_safwa@live.concordia.ca>
2023-11-19 21:20:06 -0500

    instructions on customizations in formatter

--- lib/logstash-logger/formatter/splunk_message.rb
+++ lib/logstash-logger/formatter/splunk_message.rb
@@ -1,25 +1,31 @@
 module LogStashLogger
   module Formatter
     class SplunkMessage < Base
+      # customizer = ->(event){
+      #   event["source"] ="my-cool-app"
+      #   event["source_type"] ="web-app"
+      #   event["index"] ="my-app-index"
+      #   event["app_version"] =1.0
+      # }
+       #usage:LogStashLogger.new(customize_event: customizer)
+
       def initialize(customize_event:nil)
-        @source = "source"
-        @source_type = "source_type" #"_json"
-        @index = "index"
-        @app_version = "version"
+        super(customize_event:customize_event)
       end
+      
       def format_event(event)
           {
             time: event.remove('@timestamp'),
             host: event.remove('host'),
-            source: @source,
-            sourcetype: @source_type,
-            index: @index,
+            source: event.remove('source'),
+            sourcetype: event.remove('source_type'),
+            index:  event.remove('index'),
             event: {
               message: event.remove('message'),
               severity: event.remove('severity'),
             },
           fields: {
-            app_version: @app_version,
+            app_version: event.remove('app_version'),
           }
           }.to_json
       end
